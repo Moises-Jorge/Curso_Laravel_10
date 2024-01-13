ea085777dfa674c4a80d48763e862c6fec277197
Moisés Jorge <moisesjorge30@gmail.com>
2024-01-13 13:46:27 +0100

    Creation of Support model and its migration
    The purpose was to take the data from database and pass it to view and manipulate it on the blade.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index() {
-        return view('admin/supports/index');
+    public function index(Support $support) { // ...index(Support $support) <==> $support = new Support();  Creat a instance of Class "Support".
+
+        // Getting dates from database from our model "Support" that have conection with our migration "supports"
+        $supports = $support->all();
+
+        return view('admin/supports/index', compact('supports'));
     }
 }
