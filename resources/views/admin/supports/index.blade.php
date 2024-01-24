2f7688cf1b208b8f97a6fa199fef175a8293e37b
Moisés Jorge <moisesjorge30@gmail.com>
2024-01-24 22:38:09 +0100

    Listagem de dados com o blade

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,3 +1,25 @@
 <h1>Listagem dos suportes (duvidas)</h1>
 
 <a href="{{ route('supports.create') }}">Criar Duvida</a>
+
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th> {{-- Esse daqui eh para as acoes --}}
+    </thead>
+
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
